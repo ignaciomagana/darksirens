801e6467bd1f2dbdd1c7d3f9dcfbeebbaab92f9c
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-06-29 10:32:33 -0400

    refactor: move CLI entry points into darksirens.cli

--- setup.py
+++ setup.py
@@ -26,12 +26,12 @@ setup(
     entry_points={
         "console_scripts": [
             "darksirens_inference=darksirens.cli.inference:main",
-            "darksirens_analyze=darksirens.tool.darksirens_analyze:main",
-            "darksirens_pixelate=darksirens.tool.darksirens_pixelate:main",
-            "darksirens_skymaps_to_samples=darksirens.tool.darksirens_skymaps_to_samples:main",
-            "darksirens_build_lognormal_completion=darksirens.tool.darksirens_build_lognormal_completion:main",
-            "darksirens_diagnose_lognormal_completion=darksirens.tool.darksirens_diagnose_lognormal_completion:main",
-            "darksirens_inference_lensing=darksirens.tool.darksirens_inference_lensing:main",
+            "darksirens_analyze=darksirens.cli.analyze:main",
+            "darksirens_pixelate=darksirens.cli.pixelate:main",
+            "darksirens_skymaps_to_samples=darksirens.cli.skymaps_to_samples:main",
+            "darksirens_build_lognormal_completion=darksirens.cli.build_lognormal_completion:main",
+            "darksirens_diagnose_lognormal_completion=darksirens.cli.diagnose_lognormal_completion:main",
+            "darksirens_inference_lensing=darksirens.cli.inference_lensing:main",
         ]
     },
     classifiers=[
